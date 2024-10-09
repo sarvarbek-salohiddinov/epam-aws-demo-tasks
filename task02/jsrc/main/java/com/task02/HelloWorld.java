768be40a286f548632797088dd5d505601f1c0b3
Sarvarbek Salohiddinov <5ar7ar@mail.ru>
2024-10-09 22:47:27 +0500

    task02-2nd-try

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,10 +3,16 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
+
+
+
 
 @LambdaHandler(
     lambdaName = "hello_world",
@@ -15,33 +21,36 @@ import java.util.Map;
 	aliasName = "learn",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+@LambdaUrlConfig(
+		authType= AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED)
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		Map<String, Object> resultMap = new HashMap<>();
-
-		@SuppressWarnings("unchecked")
-		Map<String, Object> requestMap = (Map<String, Object>) request;
-
-		String path = (String) requestMap.get("rawPath");
-		String method = (String) requestMap.get("httpMethod");
-
-		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
-			resultMap.put("statusCode", 200);
-			resultMap.put("message", "Hello from Lambda");
-		} else {
-			resultMap.put("statusCode", 400);
-			resultMap.put("message", String.format(
-					"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method));
-		}
-
-		return resultMap;
-
-
-//		System.out.println("Hello from lambda");
-//		Map<String, Object> resultMap = new HashMap<String, Object>();
-//		resultMap.put("statusCode", 200);
-//		resultMap.put("message", "Hello from Lambda");
+//		Map<String, Object> resultMap = new HashMap<>();
+//
+//		@SuppressWarnings("unchecked")
+//		Map<String, Object> requestMap = (Map<String, Object>) request;
+//
+//		String path = (String) requestMap.get("rawPath");
+//		String method = (String) requestMap.get("httpMethod");
+//
+//		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+//			resultMap.put("statusCode", 200);
+//			resultMap.put("message", "Hello from Lambda");
+//		} else {
+//			resultMap.put("statusCode", 400);
+//			resultMap.put("message", String.format(
+//					"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method));
+//		}
+//
 //		return resultMap;
+
+
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
+		return resultMap;
 	}
 }
