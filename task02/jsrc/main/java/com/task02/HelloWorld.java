6e950235e5d5ca41240ac5547fb192cf2036dca2
Sarvarbek Salohiddinov <5ar7ar@mail.ru>
2024-10-10 00:59:11 +0500

    task02-3rd-try-6

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -45,13 +45,47 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 //
 //		return resultMap;
 
+		Map<String, Object> parsedMap = parseObjectToMap(request);
 
-		System.out.println("Hello from lambda");
+		// Print the parsed Map
 		System.out.println(request);
+		System.out.println("================================================");
+		System.out.println(parsedMap);
+		System.out.println("================================================");
+		System.out.println(parsedMap.get("requestContext"));
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", 200);
 //		resultMap.put("message", "Hello from Lambda");
 		resultMap.put("message", request);
 		return resultMap;
+	}
+
+
+	public static Map<String, Object> parseObjectToMap(Object request) {
+		String requestString = request.toString();
+		Map<String, Object> resultMap = new HashMap<>();
+		parseKeyValuePairs(requestString, resultMap);
+
+		return resultMap;
+	}
+
+	private static void parseKeyValuePairs(String input, Map<String, Object> resultMap) {
+		input = input.replaceAll("[{}]", "");
+		String[] pairs = input.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"); // Comma outside quotes
+
+		for (String pair : pairs) {
+			String[] keyValue = pair.split("=", 2); // Split only at the first '='
+
+			if (keyValue.length == 2) {
+				String key = keyValue[0].trim();
+				String value = keyValue[1].trim();
+				if (value.startsWith("{") && value.endsWith("}")) {
+					Map<String, Object> nestedMap = new HashMap<>();
+					parseKeyValuePairs(value, nestedMap);
+					resultMap.put(key, nestedMap);
+				} else resultMap.put(key, value);
+
+			}
+		}
 	}
 }
