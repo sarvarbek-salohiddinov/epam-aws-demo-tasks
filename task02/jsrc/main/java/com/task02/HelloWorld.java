177ce29ec08982c8b452ab856c25164c88935f0d
Sarvarbek Salohiddinov <5ar7ar@mail.ru>
2024-10-10 13:15:59 +0500

    task02-3rd-try-12-6

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -43,12 +43,12 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 //                resultMap.put("statusCode", 200);
 //                resultMap.put("message", "Hello from Lambda");
                 resultMap.put("statusCode", 200);
-                resultMap.put("body", "{\"message\": \"Hello from Lambda\"}");
+                resultMap.put("body", "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
 //                resultMap.put("body", respBody);
             } else {
 //                Map<String, Object> respBody = new HashMap<>();
                 resultMap.put("statusCode", 400);
-                resultMap.put("body", "{\"message\": \"" + String.format(
+                resultMap.put("body", "{\"statusCode\": 400, \"message\": \"" + String.format(
                         "Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", rawPath, method) + "\"}");
 //                resultMap.put("body", respBody);
 
@@ -62,7 +62,7 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
         } catch (Exception e) {
 //            Map<String, Object> respBody = new HashMap<>();
             resultMap.put("statusCode", 500);
-            resultMap.put("body", "{message\": {Internal Server Error: " + e.getMessage() + "\"}");
+            resultMap.put("body", "{\"statusCode\": 500, message\": {Internal Server Error: " + e.getMessage() + "\"}");
 //            resultMap.put("body", respBody);
         }
 
