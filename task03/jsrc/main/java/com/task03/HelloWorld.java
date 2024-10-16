e97f94c427c2a5b263ab6a12b2c850de7df29a65
Sarvarbek Salohiddinov <5ar7ar@mail.ru>
2024-10-16 10:49:53 +0500

    task03-1st-impl-2

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -3,9 +3,13 @@ package com.task03;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @LambdaHandler(
@@ -21,10 +25,39 @@ import java.util.Map;
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
+		Map<String, Object> resultMap = new LinkedHashMap<>();
+
+		try {
+			@SuppressWarnings("unchecked")
+			Map<String, Object> requestMap = (Map<String, Object>) request;
+
+			String rawPath = (String) requestMap.get("rawPath");
+
+			@SuppressWarnings("unchecked")
+			Map<String, Object> requestContext = (Map<String, Object>) requestMap.get("requestContext");
+
+			@SuppressWarnings("unchecked")
+			Map<String, Object> httpContext = (Map<String, Object>) requestContext.get("http");
+
+			String method = (String) httpContext.get("method");
+
+			if ("/hello".equals(rawPath) && "GET".equalsIgnoreCase(method)) {
+				resultMap.put("statusCode", 200);
+				resultMap.put("body", "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
+			} else {
+				resultMap.put("statusCode", 400);
+				resultMap.put("body", "{\"statusCode\": 400, \"message\": \"" + String.format(
+						"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", rawPath, method) + "\"}");
+			}
+		} catch (Exception e) {
+			resultMap.put("statusCode", 500);
+			resultMap.put("body", "{\"statusCode\": 500, message\": {Internal Server Error: " + e.getMessage() + "\"}");
+		}
 		return resultMap;
+//		System.out.println("Hello from lambda");
+//		Map<String, Object> resultMap = new HashMap<String, Object>();
+//		resultMap.put("statusCode", 200);
+//		resultMap.put("message", "Hello from Lambda");
+//		return resultMap;
 	}
 }
