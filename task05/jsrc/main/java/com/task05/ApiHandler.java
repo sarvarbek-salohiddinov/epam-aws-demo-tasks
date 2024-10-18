7ce756753da0d22701c44ab020318585fdafb5ac
Sarvarbek Salohiddinov <5ar7ar@mail.ru>
2024-10-18 23:33:51 +0500

    task05-2nd-commit

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -1,6 +1,12 @@
 package com.task05;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
@@ -13,43 +19,86 @@ import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @LambdaHandler(
-    lambdaName = "api_handler",
-	roleName = "api_handler-role",
-	isPublishVersion = true,
-	aliasName = "learn",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
+		lambdaName = "api_handler",
+		roleName = "api_handler-role",
+		isPublishVersion = true,
+		aliasName = "learn",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
-)
+		invokeMode = InvokeMode.BUFFERED)
 @DynamoDbTriggerEventSource(
 		targetTable = "Events",
-		batchSize = 10
-)
+		batchSize = 10)
 @DependsOn(
 		resourceType = ResourceType.DYNAMODB_TABLE,
-		name = "Events"
-)
+		name = "Events")
 @EnvironmentVariables(value = {
 		@EnvironmentVariable(key = "region", value = "${region}"),
-		@EnvironmentVariable(key = "target_table", value = "${target_table}")
-})
+		@EnvironmentVariable(key = "target_table", value = "${target_table}")})
 public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
+	private final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		@SuppressWarnings("unchecked")
         Map<String, Object> requestToUse = (Map<String, Object>) request;
+		LambdaLogger logger = context.getLogger();
+		logger.log("Incoming request: " + request.toString());
+
+		Map<String, Object> resultMap = new HashMap<>();
+
+		String tableName = System.getenv("target_table");
+		logger.log("Table name: " + tableName);
+
+		String createdTime = Instant.now().toString();
+		logger.log("Created time: " + createdTime);
 
 
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+		try {
+			String id = UUID.randomUUID().toString();
+			int principalId = Integer.parseInt(requestToUse.get("principalId").toString());
+			@SuppressWarnings("unchecked")
+			Map<String, String> content = (Map<String, String>) requestToUse.get("content");
+
+			Map<String, AttributeValue> item = new HashMap<>();
+			item.put("id", new AttributeValue().withS(id));
+			item.put("principalId", new AttributeValue().withN(String.valueOf(principalId)));
+			item.put("createdAt", new AttributeValue().withS(createdTime));
+			item.put("body", new AttributeValue().withM(convertContentMap(content)));
+
+			logger.log("item: " + item);
+			PutItemRequest itemRequest = new PutItemRequest().withTableName(tableName).withItem(item);
+			PutItemResult itemResult = client.putItem(itemRequest);
+			logger.log("Put Item Result: " + itemResult);
+
+			Map<String, Object> eventResp = new HashMap<>();
+			eventResp.put("id", id);
+			eventResp.put("principalId", principalId);
+			eventResp.put("createdAt", createdTime);
+			eventResp.put("body", content);
+
+			resultMap.put("statusCode", 201);
+			resultMap.put("event", eventResp);
+
+			return resultMap;
+		} catch (Exception e) {
+			context.getLogger().log("Error: " + e.getMessage());
+			resultMap.put("statusCode", 500);
+			context.getLogger().log("Error: " + e.getMessage());
+			throw new RuntimeException(e);
+		}
+	}
+
+	private Map<String, AttributeValue> convertContentMap(Map<String, String> content) {
+		Map<String, AttributeValue> contentMap = new HashMap<>();
+		for (Map.Entry<String, String> entry : content.entrySet())
+			contentMap.put(entry.getKey(), new AttributeValue(entry.getValue()));
+		return contentMap;
 	}
 }
