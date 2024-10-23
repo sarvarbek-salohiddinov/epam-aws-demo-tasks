5bdf6c4ed53940af24ddff2e76302fac8cdac3b0
Sarvarbek Salohiddinov <5ar7ar@mail.ru>
2024-10-23 23:46:35 +0500

    task08-1st-attempt

---
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -0,0 +1,49 @@
+package com.task08;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.Map;
+
+@LambdaHandler(
+		lambdaName = "api_handler",
+		roleName = "api_handler-role",
+		aliasName = "${lambdas_alias_name}",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED)
+@LambdaLayer(
+		layerName = "openmetroclient-layer",
+		libraries = {"lib/weather-sdk.jar"},
+		runtime = DeploymentRuntime.JAVA11,
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP
+)
+public class ApiHandler implements RequestHandler<Map<String, Object>, String> {
+
+	public String handleRequest(Map<String, Object> request, Context context) {
+		LambdaLogger logger = context.getLogger();
+
+		logger.log("Request: " + request.toString());
+		OpenMetroClient client = new OpenMetroClient();
+		try {
+			String weatherForecast = client.getWeatherForecast();
+			logger.log("Weather forecast: " + weatherForecast);
+			return weatherForecast;
+		} catch (Exception e) {
+			logger.log("Error: " + e.getMessage());
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ task08/jsrc/main/java/com/task08/OpenMetroClient.java
@@ -0,0 +1,25 @@
+package com.task08;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class OpenMetroClient {
+    private static final String API_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+    public String getWeatherForecast() throws Exception {
+        URL url = new URL("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m");
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder content = new StringBuilder();
+
+        String inputLine;
+        while((inputLine = in.readLine()) != null) content.append(inputLine);
+
+        in.close();
+        conn.disconnect();
+        return content.toString();
+    }
+}
